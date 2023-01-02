27a13e8c93d716d0f555d5fd48c9a7f9469380c0
Katarina <katarina.dilic@gmail.com>
2023-01-02 21:45:29 +0100

    Nov update

---
+++ biblioteka.py
@@ -0,0 +1,26 @@
+biblioteka=[]
+
+while True:
+    print("Odaberi komendu: 1-unos, 2-prikaz, 3-brisanje, >3-izlaz")
+    komanda=int(input("Unesite komandu: "))
+
+    if komanda == 1:
+        naslov=input("Unesite naslov: ")
+        autor=input("Unesite autora: ")
+        isbn=int(input("Unesite isbn: "))
+        biblioteka.append([naslov,autor,isbn])
+        print("Dodata knjiga")
+    if komanda==2:
+        for knjiga in biblioteka:
+            for detalj in knjiga:
+                print(detalj)
+    if komanda==3:
+        kljucna_rec=input("Unesite naziv knjige:")
+        for knjiga in biblioteka:
+            for detalj in knjiga:
+                if detalj==kljucna_rec:
+                    biblioteka.remove(knjiga)
+                    print("Knjiga je obrisana")
+    if komanda>=3:
+        print("Izlaz iz programa")
+        break    
--- visedimenzionalni.py
+++ visedimenzionalni.py
@@ -1,13 +1,23 @@
 proizvodi=[
-            ["iphone 11","samsung s22","xiaomi x3"],
-            ["acer", "macbook","dell"],
-            ["ipad", "samsung galaxy tab", "xiaomi tablet"]
-          ]
+             ["iphone 11","samsung s22","xiaomi x3"],
+             ["acer", "macbook","dell"],
+             ["ipad", "samsung galaxy tab", "xiaomi tablet"]
+           ]
 
-telefoni=["iphone 11","samsung s22","xiaomi x3"]
-laptopovi=["acer", "macbook","dell"]
-tableti=["ipad", "samsung galaxy tab", "xiaomi tablet"]
+# telefoni=["iphone 11","samsung s22","xiaomi x3"]
+# laptopovi=["acer", "macbook","dell"]
+# tableti=["ipad", "samsung galaxy tab", "xiaomi tablet"]
 
-# proizvodi=[telefoni, laptopovi, tableti]
+# # proizvodi=[telefoni, laptopovi, tableti]
 
-print(proizvodi[0][1])
+# print(proizvodi[0][1])
+
+# for kategorija in proizvodi:
+#     for stavka in kategorija:
+#         print(stavka)
+
+for i in range(len(proizvodi)):
+    for j in range(len(proizvodi[i])):
+        print(proizvodi[i][j])
+    
+     
